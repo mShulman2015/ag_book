3848f0ffdef2497c7863bb1cd9564344181019ea
mshulman2015 <mshulman2015@gamil.com>
2018-12-02 16:02:45 -0500

    mutli box pruning to single box

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Augmented Reality book
 # QR code generation
 Codes are generated from https://www.qr-code-generator.com/ using the `Text` tab, and `photo i` where `i` is the index of the photo (starting from 1)
 
-# To RUm
+# To Run
 python3 load_vid.py
--- vid_transform.py
+++ vid_transform.py
@@ -3,6 +3,7 @@ import json
 import cv2
 from page import Page
 import math
+from shapely.geometry import Polygon
 
 class Transformer:
     def __init__(self, booket_defenition_file):
@@ -29,11 +30,10 @@ class Transformer:
         kps, des = self.detector.detectAndCompute(gray, None)
 
         # find the best match for an image that we can see on a screen
-        # TODO: replace this with a list of all matches that are good enough
-        best_matches = None
-        best_hom_transform = None
-        best_index = 0
-        best_value = 0
+        best_matches = []
+        best_hom_transforms = []
+        page_indcies = []
+        certinties = []
         for i in range(len(self.pages)):
             # compute the key point matches
             matches = self.matcher.match(self.pages[i].des, des)
@@ -47,55 +47,81 @@ class Transformer:
             # certinty is number of inliers in homography
             certinty = sum(mask)
             # maximal check
-            if certinty > self.certinty_threshold and certinty > best_value:
-                best_matches = matches
-                best_hom_transform = hom_transform
-                best_index = i
-                best_value = certinty
+            if certinty > self.certinty_threshold:
+                best_matches.append(matches)
+                best_hom_transforms.append(hom_transform)
+                page_indcies.append(i)
+                certinties.append(certinty)
 
-        return (kps, best_matches, best_hom_transform, best_index)
+        # filter out over lapping matches
+        true_best_matches = []
+        true_best_hom_transforms = []
+        true_page_indcies = []
+        true_certinties = []
+        order = np.argsort(certinties)[::-1]
+        for index in order:
+            p1 = self.getPolCorners(best_hom_transforms, gray.shape)
+
+            interserct = False
+            for i in range(len(true_best_matches)):
+                p2 = self.getPolyCorners(true_best_hom_transforms[i], gray.shape)
+
+                if p1.intersects(p2):
+                    interserct = True
+                    break
+            if not interserct:
+                true_best_matches.append(best_matches[index])
+                true_best_hom_transforms.append(best_hom_transforms[index])
+                true_page_indcies.append(page_indcies[index])
+                true_certinties.append(certinties[index])
+
+        # return results for image cmoputations
+        return (kps, best_matches, best_hom_transforms, page_indcies, certinties)
 
     # using the transfromation place the 3D object into the image
     def compute_final_frame(self, frame, page_location_info):
-        kps, matches, hom_transform, best_index = page_location_info
+        kps, best_matches, best_hom_transforms, page_indcies, certinties = page_location_info
 
-        # if we've failed to find a match return original image
-        if matches is None:
-            return frame
+        for matches, hom_transform, best_index, certinty in zip(best_matches, best_hom_transforms, page_indcies, certinties):
 
-        # ouput option 1
-        # show matches
-        ###########################################################################################################
-        # num_to_show = min(self.best_x_matches, len(matches))
-        # return cv2.drawMatches(self.pages[best_index].original_photo, self.pages[best_index].kps, frame, kps, matches[:num_to_show], 0, flags=2)
-        ###########################################################################################################
+            # if we've failed to find a match return original image
+            if matches is None:
+                continue
 
-        # ouput option 2
-        # show outline of transform
-        ###########################################################################################################
-        # h, w = self.pages[best_index].original_photo.shape[:2]
-        # pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-        # # project corners into frame
-        # dst = cv2.perspectiveTransform(pts, hom_transform)
-        # # connect them with lines
-        # return cv2.polylines(frame, [np.int32(dst)], True, best_index * 255.0 / len(self.pages[1:]), 3, cv2.LINE_AA)
-        ###########################################################################################################
+            # ouput option 1
+            # show matches
+            ###########################################################################################################
+            # num_to_show = min(self.best_x_matches, len(matches))
+            # frame = cv2.drawMatches(self.pages[best_index].original_photo, self.pages[best_index].kps, frame, kps, matches[:num_to_show], 0, flags=2)
+            ###########################################################################################################
 
-        # output option 3
-        # show the image we are trying to overlay in the right location
-        ###########################################################################################################
-        # transform = np.matmul(hom_transform, self.pages[best_index].replacement_to_real_transform)
-        # res_img = cv2.warpPerspective(self.pages[best_index].replacement_photo, transform, (frame.shape[1], frame.shape[0]))
-        # res_mask = np.where(res_img > 0, 0, 1)
-        # return np.multiply(res_mask.astype(np.uint8), frame) + res_img
-        ###########################################################################################################
+            # ouput option 2
+            # show outline of transform
+            ###########################################################################################################
+            h, w = self.pages[best_index].original_photo.shape[:2]
+            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+            # project corners into frame
+            dst = cv2.perspectiveTransform(pts, hom_transform)
+            # connect them with lines
+            frame = cv2.polylines(frame, [np.int32(dst)], True, best_index * 255.0 / len(self.pages[1:]), 3, cv2.LINE_AA)
+            ###########################################################################################################
 
-        # output option 4
-        # project the obj onto the target image
-        ###########################################################################################################
-        projection = self.projection_matrix(self.camera_parameters, hom_transform)
-        return self.render(frame, self.pages[best_index].obj, projection, self.pages[best_index].original_photo.shape[:2])
-        ###########################################################################################################
+            # output option 3
+            # show the image we are trying to overlay in the right location
+            ###########################################################################################################
+            # transform = np.matmul(hom_transform, self.pages[best_index].replacement_to_real_transform)
+            # res_img = cv2.warpPerspective(self.pages[best_index].replacement_photo, transform, (frame.shape[1], frame.shape[0]))
+            # res_mask = np.where(res_img > 0, 0, 1)
+            # frame = np.multiply(res_mask.astype(np.uint8), frame) + res_img
+            ###########################################################################################################
+
+            # output option 4
+            # project the obj onto the target image
+            ###########################################################################################################
+            # projection = self.projection_matrix(self.camera_parameters, hom_transform)
+            # frame = self.render(frame, self.pages[best_index].obj, projection, self.pages[best_index].original_photo.shape[:2])
+            ###########################################################################################################
+        return frame
 
     def projection_matrix(self, camera_parameters, homography):
         """
@@ -139,3 +165,9 @@ class Transformer:
             cv2.fillConvexPoly(frame, dst, (137, 27, 211))
 
         return frame
+
+    def getPolyCorners(self, transform, transform_shape):
+        h, w = transform_shape
+        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+        dst = cv2.perspectiveTransform(pts, transform)
+        return Polygon([(elem[0][0], elem[0][1]) for elem in dst])
